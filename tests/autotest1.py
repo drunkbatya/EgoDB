76864df8a9098d1dcdab07ee9486173f3699bb4e
egorroshch <egorroshch@egorroshchs-MacBook-Pro.local>
2022-10-14 14:55:24 +0400

    Add new autotest1

---
+++ tests/autotest1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import requests
+import time
+import sys
+
+print("starting...")
+try:
+    response = requests.get("https://db.drunkbatya.com", headers = {'host': 'db.dunkbatya.com'}, timeout = 1)
+    sys.exit(1)
+except requests.exceptions.ReadTimeout:
+    sys.exit(0)
+
