8c3aff93e94c995ae22f80eec61c01118590ffc2
egorroshch <egorroshch@egorroshchs-MacBook-Pro.local>
2022-10-24 17:09:08 +0400

    Edited autotests

--- tests/autotest1.py
+++ tests/autotest1.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
-
-# copyright 2022
-
 import requests
-import time
 import sys
+import os
 
-print("starting...")
+print("start checking response to request with invalid host header")
 try:
-    response = requests.get("https://db.drunkbatya.com", headers = {'host': 'db.dunkbatya.com'}, timeout = 1)
+    response = requests.get("https://db.drunkbatya.com", headers = {"host": "WRONG_HOST"}, timeout = 1)
+    print(Test n1 crashed)
     sys.exit(1)
 except requests.exceptions.ReadTimeout:
-    sys.exit(0)
+    next
+
+credentials = {
+        "username": os.getenv("secrets.WEBUI_TESTING_LOGIN"),
+    "password": os.getenv("WEBUI_TESTING_PASS")
+     }
+
+print("start checking the principle of issuing cookies")
+response = requests.post("https://db.drunkbatya.com/server/login", data = {"secrets.WEBUI_TESTING_LOGIN", "WEBUI_TESTING_PASS"})
+
+if "egoSession" in response.cookies:
+    next
+else:
+    print(Test n2 crashed)
+    sys.exit(1)
+
 
