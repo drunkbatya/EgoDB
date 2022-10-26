b86ad421df2310c8e15346346698d20ff1800415
egorroshch <egorroshch@egorroshchs-MacBook-Pro.local>
2022-10-26 23:52:51 +0400

    Fixed mistakes

--- tests/autotest1.py
+++ tests/autotest1.py
@@ -3,26 +3,32 @@ import requests
 import sys
 import os
 
-print("start checking response to request with invalid host header")
-try:
-    response = requests.get("https://db.drunkbatya.com", headers = {"host": "WRONG_HOST"}, timeout = 1)
-    print("Test n1 crashed")
-    sys.exit(1)
-except requests.exceptions.ReadTimeout:
-    next
 
-credentials = {
-        "username": os.getenv("secrets.WEBUI_TESTING_LOGIN"),
-    "password": os.getenv("WEBUI_TESTING_PASS")
-     }
-
-print("start checking the principle of issuing cookies")
-response = requests.post("https://db.drunkbatya.com/server/login", data = {"secrets.WEBUI_TESTING_LOGIN", "WEBUI_TESTING_PASS"})
-
-if "egoSession" in response.cookies:
-    next
-else:
-    print("Test n2 crashed")
-    sys.exit(1)
+def check_invalid_host_header():
+    print("checking invalid host header")
+    try:
+        requests.get(
+            "https://db.drunkbatya.com", headers={"host": "WRONG_HOST"}, timeout=1
+        )
+        print("Error: function on server/main.js at line 37 isn't working!")
+        return 1
+    except requests.exceptions.ReadTimeout:
+        return 0
 
 
+def check_cookies():
+    print("checking issuing cookies")
+    response = requests.post(
+        "https://db.drunkbatya.com/server/login",
+        data=os.getenv("WEBUI_TESTING_LOGIN", "WEBUI_TESTING_PASS"),
+    )
+    if "egoSession" in response.cookies:
+        return 0
+    print("Error: function server/main.js line 45 isn't working!")
+    return 1
+
+
+if __name__ == "__main__":
+    exit_code = check_invalid_host_header()
+    exit_code = check_cookies()
+    sys.exit(exit_code)
